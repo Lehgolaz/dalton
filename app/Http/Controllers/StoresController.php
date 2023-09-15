9ca9896d71e5687cafca5beb5991a66ffadd0937
Millianen <www.milli.matti@gmail.com>
2023-09-15 20:11:00 -0300

    DALTON

---
+++ app/Http/Controllers/StoresController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stores;
+use App\Http\Requests\StoreStoresRequest;
+use App\Http\Requests\UpdateStoresRequest;
+
+class StoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStoresRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Stores $stores)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Stores $stores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStoresRequest $request, Stores $stores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Stores $stores)
+    {
+        //
+    }
+}
